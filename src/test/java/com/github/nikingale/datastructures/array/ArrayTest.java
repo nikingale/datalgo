f533ee4490bb7472e649d278b47654b0e3b04317
Nikhil <nikhilingale1998@gmail.com>
2021-02-09 23:24:18 +0530

    Update Array implementation

---
+++ src/test/java/com/github/nikingale/datastructures/array/ArrayTest.java
@@ -0,0 +1,26 @@
+package com.github.nikingale.datastructures.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Nikhil Ingale 01-01-2021
+ */
+
+public class ArrayTest {
+
+    int[] array = {10, 20, 30};
+    Array test = new Array(array);
+
+    @Test
+    public void testSearchArray() {
+        int expected = 0;
+        int actual = test.searchArray(10);
+        Assert.assertEquals(expected, actual);
+
+        expected = -1;
+        actual = test.searchArray(1000);
+        Assert.assertEquals(expected, actual);
+    }
+
+}
