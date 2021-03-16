6274c780829a15ed048c416e7b2ccbfe5105c2df
Nikhil <nikhilingale1998@gmail.com>
2021-03-16 23:56:03 +0530

    Add Heap Interface

---
+++ src/main/java/com/github/nikingale/datastructures/Heap.java
@@ -0,0 +1,19 @@
+package com.github.nikingale.datastructures;
+
+import java.util.List;
+
+/**
+ * @author Nikhil Ingale 07-01-2021
+ */
+
+public interface Heap {
+
+    List<Integer> breadthFirstTraversal();
+
+    boolean search(int element);
+
+    void add(int element);
+
+    void remove(int element);
+
+}
