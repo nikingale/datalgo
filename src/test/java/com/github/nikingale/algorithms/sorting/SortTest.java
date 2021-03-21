ea08a9c7ba5ca82d9f63df5be927489fe04ab9c6
Nikhil <nikhilingale1998@gmail.com>
2021-03-21 19:56:08 +0530

    Add Sort Test

---
+++ src/test/java/com/github/nikingale/algorithms/sorting/SortTest.java
@@ -0,0 +1,56 @@
+package com.github.nikingale.algorithms.sorting;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Nikhil Ingale 10-01-2021
+ */
+
+public class SortTest {
+
+    private int[] input = {5, 3, 4, 2, 1};
+    private int[] expected = {1, 2, 3, 4, 5};
+
+    @Before
+    public void setup() {
+        input = new int[]{5, 3, 4, 2, 1};
+    }
+
+    @Test
+    public void testBubbleSort() {
+        BubbleSort sort = new BubbleSort();
+        sort.bubbleSort(input);
+        Assert.assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testInsertionSort() {
+        InsertionSort sort = new InsertionSort();
+        sort.insertionSort(input);
+        Assert.assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testSelectionSort() {
+        SelectionSort sort = new SelectionSort();
+        sort.selectionSort(input);
+        Assert.assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testMergeSort() {
+        MergeSort sort = new MergeSort();
+        int[] actual = sort.mergeSort(input);
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testQuickSort() {
+        QuickSort sort = new QuickSort();
+        sort.quickSort(input);
+        Assert.assertArrayEquals(expected, input);
+    }
+
+}
